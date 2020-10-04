42362aca69b4bb1d5e71ee3e26e77c368f7e25bb
gissehel <public-github-dk@gissehel.org>
2020-10-04 18:37:02 +0200

    TEST : Add more tests

--- src/client/reducers/geomap.test.js
+++ src/client/reducers/geomap.test.js
@@ -1,4 +1,4 @@
-import { SET_COORD } from '../constants/geomap'
+import { SET_COORD, SET_STYLE, SET_ZOOM } from '../constants/geomap'
 import geomap from './geomap'
 
 const dummyState = {
@@ -61,6 +61,135 @@ describe(`geomap reducer`, () => {
                 }
             )
         })
-
+    })
+    describe(`SET_ZOOM action`, () => {
+        it(`should set zoom in state`, async () => {
+            const state = geomap(dummyState, { type: SET_ZOOM, zoom: 17 });
+            expect(state).toEqual({
+                lat: 0.5,
+                lon: 0.7,
+                zoom: 17,
+                style: 'fancy',
+                address: null,
+                addrcoord: null,
+                marks: [],
+                addresses: [],
+                useMilliGraticule: false,
+                popupStatus: false,
+            })
+        })
+    })
+    describe(`SET_STYLE action`, () => {
+        it(`should set style in state`, async () => {
+            const state = geomap(dummyState, { type: SET_STYLE, style: 'grut' });
+            expect(state).toEqual({
+                lat: 0.5,
+                lon: 0.7,
+                zoom: 4,
+                style: 'grut',
+                address: null,
+                addrcoord: null,
+                marks: [],
+                addresses: [],
+                useMilliGraticule: false,
+                popupStatus: false,
+            })
+        })
+    })
+    describe(`LOCATION_CHANGE action`, () => {
+        const defaultAction = {
+            type: "@@router/LOCATION_CHANGE",
+            payload: {
+                location: {
+                    pathname: "/",
+                    search: "",
+                    hash: "#style=grut&map=12/48.8565834593617/2.363348007202149"
+                },
+                action: "POP",
+                isFirstRendering: false
+            }
+        }
+        it(`should modify all properties`, async () => {
+            const state = geomap(dummyState, defaultAction)
+            expect(state).toEqual({
+                lat: 48.8565834593617,
+                lon: 2.363348007202149,
+                nlat: 48.856583,
+                nlon: 2.363348,
+                zoom: 12,
+                style: 'grut',
+                address: null,
+                addrcoord: null,
+                marks: [
+                    {
+                        "lat": 48.856,
+                        "lon": 2.363,
+                    },
+                    {
+                        "lat": 48.857,
+                        "lon": 2.363,
+                    },
+                    {
+                        "lat": 48.856,
+                        "lon": 2.364,
+                    },
+                    {
+                        "lat": 48.857,
+                        "lon": 2.364,
+                    }
+                ],
+                addresses: [],
+                useMilliGraticule: false,
+                popupStatus: false,
+            })
+        })
+        it(`should be able to modify only the style`, async () => {
+            const state = geomap(dummyState, {
+                ...defaultAction,
+                payload: {
+                    ...defaultAction.payload,
+                    location: {
+                        ...defaultAction.payload.location,
+                        hash: '#style=grut'
+                    }
+                }
+            })
+            expect(state).toEqual({
+                lat: 0.5,
+                lon: 0.7,
+                zoom: 4,
+                style: 'grut',
+                address: null,
+                addrcoord: null,
+                marks: [],
+                addresses: [],
+                useMilliGraticule: false,
+                popupStatus: false,
+            })
+        })
+        it(`should be able to modify only the zoom`, async () => {
+            const state = geomap(dummyState, {
+                ...defaultAction,
+                payload: {
+                    ...defaultAction.payload,
+                    location: {
+                        ...defaultAction.payload.location,
+                        hash: '#map=15//'
+                    }
+                }
+            })
+            expect(state).toEqual({
+                lat: 0.5,
+                lon: 0.7,
+                zoom: 15,
+                style: 'fancy',
+                address: null,
+                addrcoord: null,
+                marks: [],
+                addresses: [],
+                useMilliGraticule: false,
+                popupStatus: false,
+            })
+        })
     })
 })
