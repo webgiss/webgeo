dafca77bc6a9c409514e5735c5b15c90d7ef76be
gissehel <public-dev-git-capella@gissehel.org>
2025-05-28 12:50:36 +0200

    BUGFIX : Fix basename

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -4,9 +4,11 @@ import 'semantic-ui-css/semantic.min.css'
 import MainPage from '@/components/MainPage'
 import LocationListener from '@/components/Routes/LocationListener'
 
+const basename = import.meta.env.VITE_BASE_PATH;
+
 export default () => {
     return (
-        <HistoryRouter future={{v7_startTransition: true, v7_relativeSplatPath: true}} history={history}>
+        <HistoryRouter future={{v7_startTransition: true, v7_relativeSplatPath: true}} history={history} basename={basename}>
             <LocationListener />
             <Routes>
                 <Route path='/' element={<MainPage />} />
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'node:path';
 import react from '@vitejs/plugin-react'
 // import commonjs from '@rollup/plugin-commonjs'
 
-export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: 'dist',
-    },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src', 'client'),
+
+export default ({ mode }) => {
+
+    const env = loadEnv(mode, process.cwd())
+    const basePath = env.VITE_BASE_PATH || '/'
+
+    return defineConfig({
+        base: basePath,
+        plugins: [react()],
+        build: {
+            outDir: 'dist',
         },
-    },
-    server: {
-        watch: {
-            // Watch for changes in .jsx files
-            ignored: [],
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, 'src', 'client'),
+            },
         },
-    },
-    define: {
-        // By default, Vite doesn't include shims for NodeJS/
-        // necessary for segment analytics lib to work
-        global: {},
-    },
-})
+        server: {
+            watch: {
+                // Watch for changes in .jsx files
+                ignored: [],
+            },
+        },
+        define: {
+            // By default, Vite doesn't include shims for NodeJS/
+            // necessary for segment analytics lib to work
+            global: {},
+        },
+    })
+}
